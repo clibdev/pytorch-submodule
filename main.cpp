31adc1fbfdc0be2fdfaa2a17ca3b6e21b2d9b73f
clibdev <clibdev@gmail.com>
2025-09-20 09:15:45 +0300

    update example

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <torch/torch.h>
 
 int main() {
-    torch::Tensor tensor = torch::linspace(0.00085, 0.012, 1000, torch::TensorOptions().dtype(torch::kFloat64));
+    torch::Tensor tensor = torch::linspace(0.00085, 0.012, 1000, torch::kFloat64);
     auto data = tensor.data_ptr<double>();
     for (int i = 0; i < tensor.size(0); ++i) {
         std::cout << data[i] << std::endl;
